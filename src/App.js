bba54ef008363aa80635ca47be68d3bcc437a956
Ahn <asy3310@gmail.com>
2023-03-08 17:09:38 +0900

    6.3까지 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import React, { useState, useEffect } from "react";
 import Button from "./Button";
 import styled from "./App.module.css"
 
-function App() {
 
+function App() {
+  const [counter, setValue] = useState(0) 
+  const [keyword, setKeyword] = useState("")
+  const onClick = () =>  setValue((prev) => prev + 1)
+  const onChange = (e) => {setKeyword(e.target.value)}
+  useEffect(() => {
+    console.log("11")
+  }, []);
+  useEffect(() => {
+    console.log("22")
+  }, [counter]);
+  useEffect(() => {
+    console.log("33")
+  }, [keyword]);
+  useEffect(() => {
+    console.log("4")
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styled.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
