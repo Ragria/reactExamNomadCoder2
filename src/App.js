13474cbedd42537531bb77b91dc1c1a204e8b9ed
Ahn <asy3310@gmail.com>
2023-03-10 16:10:07 +0900

    7.4장 끝

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React, { useState, useEffect } from "react";
+import Movie from "./components/Movie";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([])
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json)
-        setCoins(json);
-        setLoading(false)
-      });
-  }, [])
-  return (
-    <div>
-      <h1>The Coins!</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <select>
-        {coins.map((coin) => <option key={coin.id}>{coin.name}({coin.symbol}) : ${coin.quotes.USD.price}</option>)}
-      </select>
-    </div>
-  );
+  return null;
 }
 
 export default App;
