8951624ebd774c5a7bf57abfd08164bbdb46120b
Ragria <asy3310@gamail.com>
2023-04-03 21:36:24 +0900

    7.7 완료

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {BrowserRouter, useState, useEffect } from "react";
 import Movie from "./components/Movie";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail"
@@ -10,9 +10,9 @@ function App() {
   return (
   <Router>
     <Routes>
-      <Route path="/movie/:id" element={<Detail/>}>  
+      <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail/>}>  
       </Route>
-      <Route path="/" element={<Home/>}>  
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>}>  
       </Route>
     </Routes>
   </Router>
